6973b1905e3c62a05bc16bec6190808930fc6635
Brian <BrianLamWC@gmail.com>
2021-04-17 15:55:42 +0800

    added line 10

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import {handleSignin} from './controllers/signin.js';
 import {handleProfile} from './controllers/profile.js';
 import {handleAPICall, handleImage} from './controllers/image.js';
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
     client: 'pg',
